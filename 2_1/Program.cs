14aee6fe18919d7d679e6cf620c8c77542a84d82
Kalinin <9776020@mail.ru>
2023-03-01 01:07:14 +0300

    Seminar Task2DoneByMyself

--- 2_1/Program.cs
+++ 2_1/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿int BiggestNum(int rand_num)
+{
+  int first_num = rand_num / 10;
+  int second_num = rand_num % 10;
+  int biggest_Num = 0;
+  if (first_num > second_num) biggest_Num = first_num;
+  else biggest_Num = second_num;
+  return biggest_Num;
+}
+int enter_num = new Random().Next(1, 100);
+Console.WriteLine(enter_num);
+Console.WriteLine(BiggestNum(enter_num));
