57bfdbd9546cffe4d185c2b4a21ee0bdfb087e32
Kalinin <9776020@mail.ru>
2023-03-02 01:27:55 +0300

    HW Task1_3

---
+++ HW/1_3/Program.cs
@@ -0,0 +1,15 @@
+﻿int third_number(int num)
+{
+  if(num < 100)
+  {
+    Console.WriteLine("The number is under 100 please enter correct number");
+    return -1;
+  }
+  while(num > 999)
+  {
+    num /= 10;
+  }
+  return num % 10;
+}
+
+Console.WriteLine(third_number(int.Parse(Console.ReadLine())));
