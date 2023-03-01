2c482072d75f255dad0cd362dbd9d868f03464eb
Kalinin <9776020@mail.ru>
2023-03-02 00:44:26 +0300

    HW Task1_2 It worked?????

---
+++ HW/1_1_2/Program.cs
@@ -0,0 +1,9 @@
+﻿void Second_num(int num)
+{
+  string int_to_str = Convert.ToString(num);
+  char second_num = int_to_str[1];
+  Console.WriteLine(second_num);
+}
+int enter_num = new Random().Next(100, 1000);
+Console.WriteLine(enter_num);
+Second_num(enter_num);
