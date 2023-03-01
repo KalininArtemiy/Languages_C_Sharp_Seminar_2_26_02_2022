23873293e4032f979879ea2da4362eeaf31c2760
Kalinin <9776020@mail.ru>
2023-03-02 00:17:00 +0300

    HW Task1

---
+++ HW/1_1/Program.cs
@@ -0,0 +1,8 @@
+﻿void Second_num(int num)
+{
+  int result = ((num % 100)/10);
+  Console.WriteLine(result);
+}
+int enter_num = new Random().Next(100, 1000);
+Console.WriteLine(enter_num);
+Second_num(enter_num);
